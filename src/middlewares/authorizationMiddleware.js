b9bcc76107b997bac0094538bc9d3c4f773e8a31
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-03-07 12:51:46 -0300

    Feat: auth middleware

---
+++ src/middlewares/authorizationMiddleware.js
@@ -0,0 +1,20 @@
+import { getSessionRepository } from "../repositories/userRepository.js";
+
+async function userAuthorization(req, res, next){
+    const token = req.headers.authorization?.replace('Bearer ', '');
+    if(!token) return res.sensStatus(401);
+
+    try{
+        const {rows, rowCount} = await getSessionRepository(token);
+        if(rowCount === 0) return res.sensStatus(401);
+
+        res.locals.userId = rows[0];
+
+    }catch(err){
+        return res.status(500).send(err.message);
+
+    }
+    next();
+}
+
+export default userAuthorization;
