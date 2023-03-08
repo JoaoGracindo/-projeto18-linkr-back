e246bd3925d6a63dea2d05a27eabb4a484356ba2
rezendematheus <matheusreisrezende.2002@gmail.com>
2023-03-08 09:56:57 -0300

    organizating routes folders and app imports

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from "cors"
-import authRouter from './routers/authRouters.js';
+import { routes } from './routes/routes.js';
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
-app.use([authRouter])
+app.use([routes])
 
 
 app.listen(5000, () => console.log('Server is running on port 5000'));
