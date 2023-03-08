550e94f006c3aa1a541bfb983e464c28c77edbae
Renan Damascena Pereira <renandamascena@gmail.com>
2023-03-08 19:28:37 -0300

    fix: signUp

--- src/middlewares/validateSignUp.js
+++ src/middlewares/validateSignUp.js
@@ -1,4 +1,4 @@
-import connection from "../database/database"
+import connection from "../database/database.js"
 
 export async function validateSignUp(req,res,next){
 
@@ -7,8 +7,6 @@ export async function validateSignUp(req,res,next){
     try {
         
         const { rows:user } = await connection.query(`SELECT * FROM "users" WHERE email = $1`, [email])
-
-        console.log("chegou no validate signUp")
 
         if(user[0]) return res.status(422).send("email invalido")
 
