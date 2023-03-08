2cf95fcfa8a271f5d18fd78eb465840319f63fcb
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-03-08 17:50:05 -0300

    Feat: post link route

---
+++ src/controllers/postControllers.js
@@ -0,0 +1,16 @@
+import {insertPostRepository} from "../repositories/postRepository.js";
+
+export async function postLinkController(req, res){
+
+    const {userId} = res.locals;
+    const {link} = req.body;
+    const description = req.body.description ?? null;
+
+    try{
+        await insertPostRepository(userId, link, description);
+        return res.sendStatus(201);
+
+    }catch(err){
+        return res.status(500).send(err.message);
+    }
+}
