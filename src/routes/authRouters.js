3f84342e194cff413e2dcf5a8769cf0e5f8ac329
Renan Damascena Pereira <renandamascena@gmail.com>
2023-03-08 23:04:37 -0300

    feat/signInRouter

--- src/routes/authRouters.js
+++ src/routes/authRouters.js
@@ -1,11 +1,13 @@
 import { Router } from "express"
-import { signUp } from "../controllers/authControllers.js"
+import { signUp, signIn } from "../controllers/authControllers.js"
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { validateSignUp } from "../middlewares/validateSignUp.js"
+import { validateSignIn } from "../middlewares/validateSignIn.js"
 import signUpSchema from "../schemas/signUpSchema.js"
 
 const authRouter = Router()
 
 authRouter.post("/signup",validateSignUp, validateSchema(signUpSchema),signUp)
+authRouter.post("/signIn",validateSignIn,signIn)
 
 export default authRouter
