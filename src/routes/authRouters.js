ec95d325497c5516fff2081f91f207e3918045cf
Renan Damascena Pereira <renandamascena@gmail.com>
2023-03-09 16:50:46 -0300

    feat:logout

--- src/routes/authRouters.js
+++ src/routes/authRouters.js
@@ -1,13 +1,15 @@
 import { Router } from "express"
-import { signUp, signIn } from "../controllers/authControllers.js"
+import { signUp, signIn, logout } from "../controllers/authControllers.js"
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { validateSignUp } from "../middlewares/validateSignUp.js"
 import { validateSignIn } from "../middlewares/validateSignIn.js"
+import userAuthorization from "../middlewares/authorizationMiddleware.js"
 import signUpSchema from "../schemas/signUpSchema.js"
 
 const authRouter = Router()
 
-authRouter.post("/signup",validateSignUp, validateSchema(signUpSchema),signUp)
-authRouter.post("/signIn",validateSignIn,signIn)
+authRouter.post("/sign-up",validateSignUp, validateSchema(signUpSchema),signUp)
+authRouter.post("/sign-in",validateSignIn,signIn)
+authRouter.delete("/logout",userAuthorization,logout)
 
 export default authRouter
