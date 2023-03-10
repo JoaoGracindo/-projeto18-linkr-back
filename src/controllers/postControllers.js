584099bea098f370e9086f156dc5eb4459cdb205
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-03-10 20:35:09 -0300

    save

--- src/controllers/postControllers.js
+++ src/controllers/postControllers.js
@@ -1,9 +1,11 @@
+import urlMetadata from 'url-metadata';
+
 import {insertPostRepository, getTimelineRepository, putLinkRepository, deleteLinkRepository} from "../repositories/postRepository.js";
 
 export async function getTimelineController(req, res){
     try{
         const {rows: timeline} = await getTimelineRepository();
-
+        
         return res.status(200).send(timeline);
     }catch(err){
         return res.status(500).send(err.message);
@@ -16,8 +18,6 @@ export async function postLinkController(req, res){
     const {userId} = res.locals;
     const {link} = req.body;
     const description = req.body.description ?? null;
-    const meta = await urlMetadata(link)
-    console.log(meta)
 
     try{
         await insertPostRepository(userId, link, description);
