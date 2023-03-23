aa2cf559eab7b86a49ba35ff3b868108cdcbb907
rezendematheus <matheusreisrezende.2002@gmail.com>
2023-03-23 19:52:01 -0300

    fix/getPostsByUserId && fix/all to snake_case

--- src/repositories/getUsersRepository.js
+++ src/repositories/getUsersRepository.js
@@ -3,3 +3,11 @@ import db from "../database/database.js";
 export async function getUsersRepository(name) {
   return db.query("SELECT * FROM users WHERE name LIKE $1;", [`%${name}%`]);
 }
+
+export async function getUserByIdRepository(id){
+  return db.query(`
+      SELECT * 
+      FROM users
+      WHERE name = $1;
+    `, [id])
+}
