c4dc8d785389c9b329df085cd3cfe4fb13f392b0
Renan Damascena Pereira <renandamascena@gmail.com>
2023-03-25 21:11:45 -0300

    feat: repost_count

--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -17,16 +17,16 @@ export async function getTimelineRepository(refresh_type, timestamp) {
   if(refresh_type === "bottom") time_filter = `AND p.created_at < ${timestamp}` 
   if(refresh_type === "top")  time_filter = `AND p.created_at > ${timestamp}` 
   return await db.query(`
-
-  SELECT p.owner, p.link, p.description, p.id, p.reposted_by, p.origin_post_id, users.pic_url, users.name,
-  repost_user.name AS reposted_by_name
-  FROM posts p
-  JOIN users ON users.id = p.owner
-  LEFT JOIN users AS repost_user ON repost_user.id = p.reposted_by
-  WHERE p.deleted = false
-  GROUP BY p.id, users.pic_url, users.name, repost_user.name
-  ORDER BY p.created_at DESC
-  LIMIT 10;
+    SELECT p.owner, p.link, p.description, p.id, p.reposted_by, p.origin_post_id, users.pic_url, users.name,
+    repost_user.name AS reposted_by_name,
+    (SELECT COUNT(*) FROM posts WHERE origin_post_id = p.id) as repost_count
+    FROM posts p
+    JOIN users ON users.id = p.owner
+    LEFT JOIN users AS repost_user ON repost_user.id = p.reposted_by
+    WHERE p.deleted = false
+    GROUP BY p.id, users.pic_url, users.name, repost_user.name
+    ORDER BY p.created_at DESC
+      LIMIT 10;
     `);
 }
 
